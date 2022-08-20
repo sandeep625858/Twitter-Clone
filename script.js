dbf00f266202136e9484f59ee736a1a9bc36e024
sandeep625858 <sandeep625858@gmail.com>
2022-08-20 20:26:02 +0530

    PORT changed

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/callback",
+  callbackURL: `${process.env.BASEURL}/auth/google/callback`,
   passReqToCallback: true
 },
   function (request, accessToken, refreshToken, profile, done) {
